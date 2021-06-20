c38d9ab8e1ca42b05676d94da9a39eea00adb11e
Diego Martinez <diego777marz@gmail.com>
2021-06-20 14:46:07 -0500

    [modified] Registro de pacientes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { StatisticsComponent } from './components/statistics/statistics.component';
 import { IniciosesionComponent } from './components/iniciosesion/iniciosesion.component';
+import { RegistroPacientesComponent } from './components/registro-pacientes/registro-pacientes.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
   { path: 'inicio', component: HomeComponent },
   { path: 'estadisticas', component: StatisticsComponent },
-  { path: 'iniciosesion', component: IniciosesionComponent }
+  { path: 'iniciosesion', component: IniciosesionComponent },
+  { path: 'registro_paciente', component: RegistroPacientesComponent }
 ];
 
 @NgModule({
